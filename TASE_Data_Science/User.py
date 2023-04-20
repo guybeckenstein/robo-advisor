c7ab98113eb67602e243a4bd2b63f0be0fec8f77
Yarden Agami <yardenagami@gmail.com>
2023-04-20 20:38:23 +0300

    updated files

---
+++ TASE_Data_Science/User.py
@@ -0,0 +1,36 @@
+
+import manageData 
+#TODO -BUILD USER CLASS
+
+class User:
+    level=1
+    name=""
+    sybmolIndex= []
+    portfolio = []
+    
+
+    def __init__(self,name="",level=1,sybmolIndex=[]):
+        User.name = name
+        User.level = level
+        User.sybmolIndex = sybmolIndex
+
+        
+    def getName(self):
+        return User.name
+    def getLevel(self):
+        return User.level
+    def getSybmolIndex(self):
+        return User.sybmolIndex
+    
+    def getPortfolioData(self):
+        return User.portfolio
+    
+    def setPortfolioData(self,command):
+        User.portfolio=manageData.setPortfolioData(command,User.sybmolIndex)
+             
+    
+   
+
+ 
+
+    
