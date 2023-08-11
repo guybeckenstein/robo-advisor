a1b70b2fce33815904c3a646ec3beadca807522f
Yarden Agami <yardenagami@gmail.com>
2023-08-11 16:35:28 +0300

    yarden updated 11/8

--- service/download_data_for_reasearch_main.py
+++
@@ -1,7 +0,0 @@
-from util import data_management
-from service.config import settings
-
-if __name__ == '__main__':
-    # TODO filter with volume
-    data_management.download_data_for_research(settings.NUM_OF_YEARS_HISTORY)
-
