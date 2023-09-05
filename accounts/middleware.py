b3ef976a887a6d62ccb44f3498470bdf0b182233
Guy Beckenstein <guybec@mta.ac.il>
2023-09-06 02:01:26 +0300

    Fixed quick sign in issue with SITE_ID within settings.py
    Signed-off-by: Guy Beckenstein <guybec@mta.ac.il>

--- accounts/middleware.py
+++ accounts/middleware.py
@@ -1,6 +1,13 @@
 from django.contrib.sites.models import Site
 from django.conf import settings
 
+"""
+This file is mainly for fixing the potential issue when entering the login page (GET request), because of django-allauth
+The value of SITE_ID matters, thus wrong value will make this error:
+`django.contrib.sites.models.Site.DoesNotExist: Site matching query does not exist.`
+Using this file (that is linked directly to `settings.py`), we fix the problem.
+"""
+
 
 class DynamicSiteIDMiddleware:
     def __init__(self, get_response):
@@ -11,7 +18,7 @@ class DynamicSiteIDMiddleware:
             current_site = Site.objects.get(domain=request.get_host())
             settings.SITE_ID = current_site.id
         except Site.DoesNotExist:
-            settings.SITE_ID = 1  # Default to SITE_ID 1 if site doesn't exist
+            settings.SITE_ID = 1
 
         response = self.get_response(request)
         return response
