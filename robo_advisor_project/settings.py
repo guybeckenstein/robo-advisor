97790a31d66bf4d7759ccdcfddea34ef7990f4af
Guy Beckenstein <guybec@mta.ac.il>
2023-09-06 17:11:03 +0300

    Trying to figure out the issue with the github actions ci
    Signed-off-by: Guy Beckenstein <guybec@mta.ac.il>

--- robo_advisor_project/settings.py
+++ robo_advisor_project/settings.py
@@ -127,7 +127,7 @@ DATABASES = {
         'NAME': os.environ.get('POSTGRES_DB', 'robo_advisor'),
         'USER': os.environ.get('POSTGRES_USER', 'postgres'),
         'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'postgres'),
-        'HOST': os.environ.get('POSTGRES_HOST', 'localhost'),
+        'HOST': 'db',  # os.environ.get('POSTGRES_HOST', 'localhost'),
         'PORT': int(os.environ.get('POSTGRES_PORT', 5432)),
     }
 }
