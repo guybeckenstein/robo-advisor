b5b8301f2a3502647384526823a0f63c6238c5c7
Guy Beckenstein <guybec@mta.ac.il>
2023-09-10 04:19:00 +0300

    Update
    Signed-off-by: Guy Beckenstein <guybec@mta.ac.il>

--- robo_advisor_project/settings.py
+++ robo_advisor_project/settings.py
@@ -1,23 +1,7 @@
 import os
-from pathlib import Path
-
-import environ
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-# Load and read .env file
-# OS environment variables take precedence over variables from .env
-env = environ.Env()
-env.read_env(os.path.join(BASE_DIR, './.env.dev'))
-
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
-# Load and read .env file
-# OS environment variables take precedence over variables from .env
-env = environ.Env()
-env.read_env(os.path.join(BASE_DIR, './.env.dev'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
