897b9cfea8647cda57b5a1a71ecffa0eb6c6a47e
Yarden Agami <yardenagami@gmail.com>
2023-09-10 10:43:22 +0300

    almost done

--- robo_advisor_project/settings.py
+++ robo_advisor_project/settings.py
@@ -1,6 +1,6 @@
 import os
 
-aws_mode=False
+aws_mode = True
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
